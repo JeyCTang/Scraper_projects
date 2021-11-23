1cae99dd291cd2a6eab1b814bcaa1185d9d68146
JeyCT <tangzhixi@gmail.com>
2021-11-23 22:43:43 +0000

    add download function

--- scrape_huya_douyu/demo.py
+++ scrape_huya_douyu/demo.py
@@ -3,6 +3,12 @@ import os
 
 import requests
 
+from urllib import request  # import for downloading the picture
+
+from tqdm import tqdm
+
+import multiprocessing  # use multiprocess to work
+
 
 def get_person(page):
     """
@@ -30,16 +36,32 @@ def get_person(page):
         extracted_info.append({"username": username, "img_url": user_image_url})
 
     # save the extract data as json format
-    with open('extract_person.json', 'a') as f:
+    with open(f'extract_person_{page}.json', 'a') as f:
         content = json.dumps(extracted_info, indent=4)
         f.write(content)
 
 
 def download_image(json_file):
     with open(json_file, 'r') as f:
-        content = f.read()
+        content = json.loads(f.read())
 
+    count = 0
+    for person in tqdm(content):
+        # automatically create a new directory
+        if not os.path.exists("./douyu_files"):
+            os.mkdir("./douyu_files/")
+        # download
+        try:
+            request.urlretrieve(person["img_url"], f'./douyu_files/{person["username"]}.png')
+            request.urlcleanup()  # clean the cache
+        except Exception as e:
+            # print('error:', person["username"])
+            print('error', e)
+
+
+def multiprocess(func_name, page):
+    multiprocessing.Process(target=func_name, args=page).start()
 
 
 if __name__ == '__main__':
-    get_person(1)
+
